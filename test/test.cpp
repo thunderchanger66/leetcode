7e3f456acf4964367d0b51c81e8e8fe56e588e60
thunderchanger66 <thunderchanger@outlook.com>
2025-01-20 09:14:12 +0800

    commit1_5

--- test/test.cpp
+++ test/test.cpp
@@ -9,39 +9,21 @@
 #include <stdint.h>
 //using namespace std;
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    ListNode() : val(0), next(nullptr) {}
-    ListNode(int x) : val(x), next(nullptr) {}
-    ListNode(int x, ListNode *next) : val(x), next(next) {}
-};
-
 class Solution
 {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2)
+    std::vector<int> twoSum(std::vector<int>& nums, int target)
     {
-        ListNode *head = nullptr, *tail = nullptr;
-        int carry = 0;
-        while(l1 || l2)
+        std::unordered_map<int, int> hashtable;
+        int length = nums.size();
+        for(int i = 0; i < length; i++)
         {
-            int n1 = l1 ? l1->val : 0;
-            int n2 = l2 ? l2->val : 0;
-            int sum = n1 + n2 + carry;
-            if(!head) head = tail = new ListNode(sum % 10);
-            else
-            {
-                tail->next = new ListNode(sum % 10);
-                tail = tail->next;
-            }
-            carry = sum / 10;
-            if(l1) l1 = l1->next;
-            if(l2) l2 = l2->next;
+            auto x = hashtable.find(target - nums[i]);
+            if(x != hashtable.end())
+                return {x->second, i};
+            hashtable[nums[i]] = i;
         }
-        if(carry)
-            tail->next = new ListNode(carry);
-        return head;
+        return {};
     }
 };
 
